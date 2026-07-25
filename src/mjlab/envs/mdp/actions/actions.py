4d159f0f7c9095b365f405fc4e1f17639b8507f1
Wataru Oshima <wataru.oshima@techshare.co.jp>
2026-07-25 15:10:08 +0900

    Reset processed actions to offsets on environment reset

--- src/mjlab/envs/mdp/actions/actions.py
+++ src/mjlab/envs/mdp/actions/actions.py
@@ -163,8 +163,13 @@ class BaseAction(ActionTerm):
       )
 
   def reset(self, env_ids: torch.Tensor | slice | None = None) -> None:
-    """Reset raw actions to zero for specified environments."""
+    """Reset raw and processed actions for specified environments."""
     self._raw_actions[env_ids] = 0.0
+
+    if isinstance(self._offset, torch.Tensor):
+      self._processed_actions[env_ids] = self._offset[env_ids]
+    else:
+      self._processed_actions[env_ids] = self._offset
 
 
 ##
